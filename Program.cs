dfc2d72a27fa0aee212ee13fd283dd5fc71b9a11
TechnoLatopumi <tegartpp@gmail.com>
2022-08-04 08:02:45 +0700

    Added str and num method

--- Program.cs
+++ Program.cs
@@ -2,10 +2,24 @@
 {
     private static void Main(string[] args)
     {
+        //string
         string phrase ="Giraffe\"Academy\"";
+
+        Console.WriteLine("===Basic String Method===");
+        Console.WriteLine(phrase);
         Console.WriteLine(phrase.Contains("Academy"));
         Console.WriteLine(phrase.Length);
         Console.WriteLine(phrase[3]);
+        Console.WriteLine(phrase.IndexOf("Academy"));
+        Console.WriteLine(phrase.IndexOf("f"));
+        Console.WriteLine(phrase.IndexOf("z"));
+        Console.WriteLine(phrase.Substring(8));
+        Console.WriteLine(phrase.Substring(8, 3));
+
+        //Number
+         Console.WriteLine("===Basic Number Method===");
+        Console.WriteLine(-5.783);
+        Console.WriteLine(5+8);
 
         Console.ReadLine();
     }
